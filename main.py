a6aa283ac29392154daec37748946c0c9b235540
capyBearista <capyBearista@users.noreply.github.com>
2025-10-04 10:50:53 -0400

    restructure script (the original worked fine, WSL just doesn't have native access to cameras)

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import cv2
 
-for i in range(5):
-    cap = cv2.VideoCapture(i)
-    if cap.isOpened():
-        print(f"Camera {i} is available.")
-        cap.release()
-    else:
-        print(f"Camera {i} is not available.")
-# vidcap = cv2.VideoCapture(1)
+# for i in range(5):
+#     cap = cv2.VideoCapture(i)
+#     if cap.isOpened():
+#         print(f"Camera {i} is available.")
+#         cap.release()
+#     else:
+#         print(f"Camera {i} is not available.")
+vidcap = cv2.VideoCapture(0)
 
-# if not vidcap.isOpened():
-#     print("[error] Could not open VideoCapture. Exiting...")
-#     exit()
+if not vidcap.isOpened():
+    print("[error] Could not open VideoCapture. Exiting...")
+    exit()
 
-# # loop forever
-# while True:
-#     ret, frame = vidcap.read()
+# loop forever
+while True:
+    ret, frame = vidcap.read()
+    
+    if not ret:
+        print("[warn] Could not open frame.")
+        break
 
-#     if not ret:
-#         print("[warn] Could not open frame.")
-#         break
+    cv2.imshow("Live Camera Feed", frame)
 
-#     cv2.imshow("Live Camera Feed", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-#     vidcap.release()
-#     cv2.destroyAllWindows()
+vidcap.release()
+cv2.destroyAllWindows()
